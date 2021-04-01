1d5658899cb9a06f6b2fd24e0ec5c58b303fd5ee
makeinstall77 <make_install@yahoo.com>
2021-04-01 22:46:56 +1000

    update

--- app/main.py
+++ app/main.py
@@ -17,14 +17,27 @@ html = '''<!DOCTYPE html>
  <title>ESP32 Web Server</title>
  <meta name="viewport" content="width=device-width, initialscale=1">
  <link rel="icon" href="data:,">
- <style>
- html {
- font-family: Helvetica;
- display: inline-block;
- margin: 0px auto;
- text-align: center;
- }
- </style>
+  <style>
+    html {
+      font-family: Helvetica;
+      display: inline-block;
+      margin: 0px auto;
+      text-align: center;
+    }
+    .button {
+      background-color: #4CAF50;
+      border: none;
+      color: white;
+      padding: 16px 40px;
+      text-decoration: none;
+      font-size: 30px;
+      margin: 2px;
+      cursor: pointer;
+    }
+    .button2 {
+      background-color: #555555;
+    }
+  </style>
 </head>
 <body>
  <h1>ESP32 Web Server</h1>
