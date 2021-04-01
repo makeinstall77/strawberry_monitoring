ad11ae268daf87f71120d29221ad72f1ba258664
makeinstall77 <make_install@yahoo.com>
2021-04-02 00:00:34 +1000

    update

--- app/main.py
+++ app/main.py
@@ -62,8 +62,10 @@ try:
         machine.reset()
     else:
         print("Up to date!")
-except:
-    print ("error while checking new version")
+except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print (exc_type, fname, exc_tb.tb_lineno)
     pass
 
 varVolt = 4.1339 # среднее отклонение (ищем в excel)
