6b66c3019e7fc27bc3ccbb42c564793f0953cd94
makeinstall77 <make_install@yahoo.com>
2021-04-01 21:45:23 +1000

    update

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from config import *
 from machine import Pin
 from micropyserver import MicroPyServer
 
-print ("version 0.3")
+print ("version 0.5")
 
 OTA = senko.Senko(
   user="makeinstall77", # Required
